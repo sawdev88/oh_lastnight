8f4c8896cdba29276dc902b71538c01e717bca82
Sean Wines <seanwinesdev@gmail.com>
2016-06-12 09:31:04 -0700

    Initial functionality and design

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @post = Post.new
+    @posts = Post.all.order("created_at DESC").take(20).each
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
